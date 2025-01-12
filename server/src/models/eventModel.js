0d3243d73e5d2b1e687756703841f1e191c7dfff
Prabhat Chandela <chandela.prabhat@gmail.com>
2025-01-12 23:50:58 +0530

    user, event model modification and custom methods added

--- server/src/models/eventModel.js
+++ server/src/models/eventModel.js
@@ -1,11 +1,34 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  date: { type: Date, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  name: {
+     type: String, 
+     required: true 
+  },
+
+  description: { 
+    type: String 
+  },
+
+  date: { 
+    type: Date, 
+    required: true 
+  },
+
+  owner: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+
+  attendees: [{ 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User' 
+  }],
+
+  image: { 
+    type: String 
+  }, // Cloudinary URL for the event image
 }, { timestamps: true });
 
 export default mongoose.model('Event', eventSchema);
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,10 +1,45 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: {
+     type: String, 
+     required: true, 
+     unique: true, 
+     lowercase: true,
+     trim: true, 
+     index: true 
+  },
+
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    lowercase: true,
+    trim: true, 
+  },
+
+  fullname: { 
+    type: String, 
+    required: true,
+    trim: true, 
+  },
+
+  avatar: {
+    type: String,
+    required: true
+  },
+
+  password: { 
+    type: String, 
+    required: [true, 'Password is required'] 
+  },
+
+  refreshtoken: { 
+    type: String, 
+  },
+
 }, { timestamps: true });
 
 // Hash password before saving
@@ -13,5 +48,39 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+// Custom method to validate the password
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+// Custom method to generate Access Token
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+                {
+                  _id: this._id,
+                  email: this.email,
+                  username: this.username,
+                  fullname: this.fullname
+                },
+                  process.env.ACCESS_TOKEN_SECRET,
+                {
+                  expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+                }
+  )
+}
+
+// Custom method to generate Refresh Token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+                {
+                  _id: this._id,
+                },
+                  process.env.REFRESH_TOKEN_SECRET,
+                {
+                  expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+                }
+  )
+}
 
 export default mongoose.model('User', userSchema);
